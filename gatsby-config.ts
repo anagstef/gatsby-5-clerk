b112896938df57fd77243460ec562459b76bf4f8
anagstef <anagstef@gmail.com>
2023-01-27 18:11:28 +0200

    Add Clerk to project

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,5 +1,9 @@
 import type { GatsbyConfig } from "gatsby"
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Gatsby 5 Clerk`,
@@ -9,7 +13,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: ['gatsby-plugin-clerk'],
 }
 
 export default config
